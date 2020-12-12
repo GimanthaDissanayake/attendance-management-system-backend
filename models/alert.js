7e19c35f11e5b4579cf259be9396f31113ebd09a
Gimantha <gimanthad@gmail.com>
2020-12-12 13:09:19 +0530

    fix alert

--- models/alert.js
+++ models/alert.js
@@ -3,7 +3,7 @@ const db = require('../util/database');
 module.exports = class Alert {
     static findByReciever_id(reciever_id) {
         // return alert by the lecturer id
-        return db.execute('SELECT lecturer_name , alert.alert_id , send.lecturer_id , message, date_time FROM alert, lecturer , send WHERE alert.alert_id = send.alert_id AND lecturer.lecturer_id = send.lecturer_id AND receiver_id=? ORDER BY date_time DESC ', [reciever_id] );
+        return db.execute('SELECT lecturer_name , alert.alert_id , send.lecturer_id , message, date_time FROM alert, lecturer WHERE alert.alert_id = send.alert_id AND lecturer.lecturer_id = send.lecturer_id AND receiver_id=? ORDER BY date_time DESC ', [reciever_id] );
     }
 
     static insertAlert(message,receiver,userId){
